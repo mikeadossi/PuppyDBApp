df49c8adb67115954290baecb34ab2bee012f7a7
mikeadossi <mikeadossi@gmail.com>
2016-11-17 11:55:33 -0800

    first commit

---
+++ database/puppies.js
@@ -0,0 +1,41 @@
+const db = require('./main')
+
+const Puppy = {
+
+  add: ( request, response, next ) => {
+
+  },
+  getAll: ( request, response, next ) => {
+    db.any('SELECT * FROM pups')
+    .then( data => {
+        response.status( 200 ).json({
+          status: 'sucess',
+          data: data,
+          message: 'Retrieved all puppies.'
+        })
+    })
+
+  },
+  getOne: ( request, response, next ) => {
+    const { id } = request.params
+    db.one( `SELECT * FROM pups WHERE id=${id}` )
+    .then( data => {
+      response.status( 200 ).json({
+      status: 'yay',
+      data: data,
+      message: 'We did a thing'
+    })
+    .catch( error => next( error ))
+  })
+
+
+  },
+  update: ( request, response, next ) => {
+
+  },
+  delete: ( request, response, next ) => {
+
+  }
+}
+
+module.exports = Puppy
