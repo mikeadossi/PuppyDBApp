5f4fb674371220ccbb14d38e9c33c9a3648176be
lizzkats <katsnelson.lizz@gmail.com>
2016-11-17 17:19:43 -0800

    Fixed front end to show send button, cleaned up route

--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,21 @@ var express = require('express');
 var router = express.Router();
 var db = require('../database/database');
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'puppyApp' });
-});
-  // response.send('Yay')
+
+router.get('/', (request, response, next) => {
+
   db.getAllPuppies()
     .then( puppies => {
-      // const indexPuppies = puppies.map( book =>
-      //   Object.assign( {}, book,
-      //     { description: book.description.slice( 0, 20 ) }
+
+      // const indexPuppies = puppies.map( puppy =>
+      //   Object.assign( {}, puppy,
+      //     { description: puppies.description.slice( 0, 20 ) }
       //   )
       // )
 
       response.render('index', { puppies }
     )
   })
-
+})
 
 module.exports = router;
